29fdca3b3a19a45d0af815fac89c6fe9d0888a5b
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2017-12-02 19:50:39 -0500

    Added support for reading directories and bzip2 files.

--- salento-to-txt.py
+++ salento-to-txt.py
@@ -1,31 +1,61 @@
 #!/usr/bin/env python3
-
 import ijson
 import sys
+import itertools
+import glob
+import os
+import bz2
 
-def prog2seq(fd):
-    for seq in ijson.items(fd, 'packages.item.data.item.sequence'):
-        yield(list(x['call'] for x in seq))
+def find_files(dirname, ext):
+    return glob.glob(os.path.join(dirname, "**", ext), recursive=True)
+
+def find_sal(dirname):
+    return itertools.chain(
+        find_files(dirname, "*.sal"),
+        find_files(dirname, "*.sal.bz2")
+    )
 
 class sequences:
-    def __init__(self, fd):
+    def __init__(self, fd, include_packages=True):
         self.fd = fd
+        self.include_packages = include_packages
+
     def __iter__(self):
-        return prog2seq(self.fd)
+        query = 'data.item.sequence'
+        if self.include_packages:
+            query = 'packages.item.' + query
+        for seq in ijson.items(self.fd, query):
+            yield(list(x['call'] for x in seq))
 
 def main():
     import argparse
     parser = argparse.ArgumentParser(description="Converts a Salento JSON dataset into plain text.")
-    parser.add_argument("-i", dest="infile", nargs='?', type=argparse.FileType('rb'),
-                     default=sys.stdin, help="A file of the Salento Dataset format.")
-             
+    parser.add_argument("-i", dest="infile", nargs='?', type=str,
+                     default="/dev/stdin", help="A file of the Salento Dataset format.")
+    parser.add_argument("-d", dest="dir", nargs='?', type=str,
+                     default=None, help="A directory containing Salento JSON Package. Default: standard input.")
     parser.add_argument("-o", dest="outfile", nargs='?', type=argparse.FileType('w'),
                      default=sys.stdout, help="A file of a text format.")
     args = parser.parse_args()
 
-    for seq in sequences(args.infile):
-        if len(seq) > 0:
-            args.outfile.write(" ".join(seq) + " $END\n")
+    infiles = [args.infile]
+    include_pkgs = True
+    if args.dir is not None:
+        infiles = find_sal(args.dir)
+        include_pkgs = False
+
+    for f in infiles:
+        fopen = bz2.open if f.endswith(".bz2") else open
+        with fopen(f) as fp:
+            try:
+                for seq in sequences(fp, include_packages=include_pkgs):
+                    if len(seq) > 0:
+                        args.outfile.write(" ".join(seq) + " $END\n")
+            except ijson.common.IncompleteJSONError:
+                print("Error parsing: " + f, file=sys.stderr)
+                continue
+            print("OK " + f)
+            
 
 if __name__ == '__main__':
     main()
