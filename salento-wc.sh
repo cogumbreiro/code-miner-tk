e80717a8d05cc7c4be6a7edd1741d584fc78bb54
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2017-12-03 23:05:34 -0500

    Hide empty lines.

--- salento-wc.sh
+++ salento-wc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 home=$(dirname "$0")
-"$home"/salento-to-txt.py -f "$@" | sed -E -e 's/ /\n/g' | sort | uniq -c
+"$home"/salento-to-txt.py --eol '' -f "$@" | sed -E -e 's/ /\n/g' | sed '/^\s*$/d' | sort | uniq -c
