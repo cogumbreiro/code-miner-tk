684028922a499e5b393d294b0bea8ad0e0729420
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2018-01-04 13:36:02 -0500

    Added more metadata.

--- apisan-to-salento.py
+++ apisan-to-salento.py
@@ -21,18 +21,96 @@ except ImportError:
 
 import json
 
-from apisan.parse.event import CallEvent
+from apisan.parse.event import CallEvent, AssumeEvent
+from apisan.parse.symbol import *
 from apisan.parse import explorer
+from collections import OrderedDict
 
 __all__ = ['exec_tree_to_sequences']
 
+def get_symbol(node):
+    if isinstance(node, CallSymbol):
+        for x in node.args:
+            x = get_symbol(x)
+            if x is not None:
+                return x
+    elif isinstance(node, BinaryOperatorSymbol):
+        result = get_symbol(node.lhs)
+        if result is None:
+            result = get_symbol(node.rhs)
+        return result
+    elif isinstance(node, ArraySymbol) or isinstance(node, FieldSymbol):
+        return get_symbol(node.base)
+    elif isinstance(node, ConstraintSymbol):
+        return get_symbol(node.symbol)
+    elif isinstance(node, IDSymbol):
+        return node.id
+
+def get_symbols(evt):
+    if evt.call.kind == SymbolKind.Call:
+        for idx, node in enumerate(evt.call.children):
+            yield (get_symbol(node), idx)
+
+
+class ArgsDB:
+    def __init__(self):
+        self.last_node = None
+        self.db = []
+    
+    def push_call(self, evt):
+        if evt.call_name is not None:
+            yield from self.flush()
+            self.last_node = evt
+
+    def push_branch(self, evt):
+        if self.last_node is not None and evt._cond.text.startswith(self.last_node.call_name):
+            yield from self.flush(branch=True)
+
+    def flush(self, *args, **kwargs):
+        if self.last_node is not None:
+            yield self.evt_to_json(self.last_node, *args, **kwargs)
+        self.last_node = None
+
+    def lookup(self, name):
+        if name is not None:
+            for scope_idx, scope in enumerate(reversed(self.db)):
+                if name in scope:
+                    return scope_idx, scope[name]
+        return -1, -1
+
+    def get_args(self, evt):
+        args = OrderedDict(get_symbols(evt))
+        for name in args:
+            left, right = self.lookup(name)
+            yield left
+            yield right
+        self.db.append(args)
+
+    def evt_to_json(self, evt, branch=False):
+        states = [1 if branch else 0] + list(self.get_args(evt))
+        return {'call':evt.call_name, 'states':states, 'location':evt.code}
+
 def to_call_path(path):
+    db = ArgsDB()
+    for node in path:
+        evt = node.event
+        if isinstance(evt, CallEvent):
+            yield from db.push_call(evt)
+        elif isinstance(evt, AssumeEvent):
+            yield from db.push_branch(evt)
+        else:
+            # flush
+            yield from db.flush()
+    yield from db.flush()
+
+def to_call_path_simple(path):
     for node in path:
         evt = node.event
         if isinstance(evt, CallEvent):
             name = evt.call_name
             if name is not None:
-                yield {'call':name, 'states':[], 'location':evt.code}
+                if last_node is not None:
+                    yield evt_to_json(last_node)
 
 def exec_tree_to_sequences(exec_tree):
     """
