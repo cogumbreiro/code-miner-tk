57cab3c683c46f4f08ffa8f4fbbaca6a66e6d33b
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2018-04-06 10:55:54 -0400

    New version; added a rudimentary makefile-dependency algo.

--- salento-train.py
+++ salento-train.py
@@ -9,38 +9,61 @@ except ImportError:
     sys.path.append(path.join(home, "src"))
 
 import common
+import make
 import os.path
 import shlex
 import json
 import tarfile
+import string
+import time
+import subprocess
+from functools import reduce
 
-def get_path(args, attr, default=None):
-    fname = getattr(args, attr)
-    if fname is None:
-        fname = default
-    if fname is None or os.path.isabs(fname):
-        return fname
-    return os.path.join(args.dirname, fname)
+################################################################################
 
-def do_train(args):
+M = make.Makefile()
+
+@M.rule(target="{save_dir}")
+def init(ctx, args):
+    save_dir = ctx.get_path("{save_dir}")
+    if args.dry_run:
+        print("MKDIR: " + repr(save_dir))
+    else:
+        os.mkdir(save_dir)
+
+@M.rule(source="{save_dir}",
+targets=[
+    "{save_dir}/model.pbtxt",
+    "{save_dir}/config.json",
+    "{save_dir}/model.pb",
+    "{save_dir}/checkpoint"
+])
+def train(ctx, args):
+
     # 1. Get script path
-    train_py = os.path.join(args.salento_home, "src/main/python/salento/models/low_level_evidences/train.py")
-    cmd = "%s %s" % (shlex.quote(args.python_bin), shlex.quote(train_py))
-    
-    # 2. Get filename
-    cmd += " %s " % shlex.quote(get_path(args, "infile"))
+    cmd = [
+        args.python_bin,
+        os.path.join(args.salento_home, "src/main/python/salento/models/low_level_evidences/train.py"),
+        ctx.get_path("{infile}"),
+    ]
 
     # 3. Get configuration file
-    config = get_path(args, "config_file")
+    config = ctx.get_path("{config_file}")
     if os.path.exists(config):
-        cmd += " --config " + shlex.quote(config)
-
-    # 4. Add logging
+        cmd.append("--config")
+        cmd.append(config)
     if not args.skip_log:
-        log_file = get_path(args, "log_file")
-        cmd += " | tee " + shlex.quote(log_file)
+        log_file = ctx.get_path('{log_file}')
+        stdout = open(log_file, "w")
+    else:
+        stdout = None
+    if args.echo:
+        print(" ".join(map(shlex.quote, cmd)))
+    result = subprocess.run(cmd, stdout=stdout, stderr=subprocess.STDOUT)
 
-    common.run(cmd, silent=args.quiet, echo=args.echo, dry_run=args.dry_run)
+    if result.returncode != 0:
+        print("ERROR training", file=sys.stderr)
+        raise KeyboardInterrupt
 
 def parse_checkpoint_file(fname):
     with open(fname) as fp:
@@ -53,32 +76,34 @@ def get_save_dir_files(dirname):
     return ["model.pbtxt", "config.json", "model.pb", "checkpoint"] + \
         list(dict(parse_checkpoint_file(os.path.join(dirname, 'checkpoint'))).values())
 
-def backup_save_dir(dirname, target_filename):
+def backup_files(target_filename, dirname):
+    files = get_save_dir_files(dirname)
     tf = tarfile.open(target_filename, "w")
-    for fname in get_save_dir_files(dirname):
+    for fname in files:
         tf.add(os.path.join(dirname, fname), arcname=fname)
     tf.close()
+    
 
-def do_backup(args):
+@M.rule(sources=[
+    "{save_dir}/model.pbtxt",
+    "{save_dir}/config.json",
+    "{save_dir}/model.pb",
+    "{save_dir}/checkpoint",
+], target="{backup_file}")
+def backup(ctx, args):
     if args.skip_backup:
         return
-    save_dir = get_path(args, "save_dir")
+    save_dir = ctx.get_path("{save_dir}")
+    backup_file = ctx.get_path("{backup_file}")
     if args.dry_run:
         print("BACKUP: " + repr(save_dir))
         return
-    backup_save_dir(args.backup_file, save_dir)
-
-def do_prepare(args):
-    save_dir = get_path(args, "save_dir")
-    if args.dry_run:
-        print("MKDIR: " + save_dir)
-    else:
-        try:
-            os.mkdir(save_dir)
-        except FileExistsError:
-            if not args.force:
-                print("%r directory already exists; remove it before continuing." % save_dir, file=sys.stderr)
-                sys.exit(1)
+    try:
+        backup_files(target_filename=backup_file, dirname=save_dir)
+    except OSError as err:
+        print("Error writing backup file %r:" % backup_file, err, file=sys.stderr)
+        common.delete_file(backup_file)
+        raise KeyboardInterrupt()
 
 def main():
     import argparse
@@ -92,22 +117,27 @@ def main():
     
     parser.add_argument("--dry-run", action="store_true", help="Do not actually run any program, just print the commands.")
     parser.add_argument("--skip-log", action="store_true", help="Skip logging.")
-    parser.add_argument("--skip_backup", action="store_true", help="Skip backing up the save directory.")
+    parser.add_argument("--skip-backup", action="store_true", help="Skip backing up the save directory.")
     parser.add_argument("--force", action="store_true", help="Ignore if saves directory exists.")
     parser.add_argument("--echo", action="store_true", help="Print out commands that it is running.")
-    parser.add_argument("--quiet", action="store_true", help="Print out the output of training.")
     common.parser_add_salento_home(parser, dest="salento_home")
     parser.add_argument("--python-bin", default="python3", help="Python3 binary. Default: %(default)r")
     args = parser.parse_args()
 
     try:
-        do_prepare(args)
-        do_train(args)
-        do_backup(args)
+        ctx = make.FileCtx(make.EnvResolver(args.dirname, vars(args)))
+        try:
+            if args.skip_backup:
+                M.run(ctx, [train], args)
+            else:
+                M.make(ctx, args, target="{backup_file}")
+        except ValueError as e:
+            print(e, file=sys.stderr)
+            sys.exit(1)
+        #do_backup(ctx)
     except KeyboardInterrupt:
         sys.exit(1)
     
 if __name__ == '__main__':
     main()
  
-
---
+++ src/make.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python3
+import os.path
+import string
+import time
+import subprocess
+from functools import reduce
+
+def resolve_path(dirname, fname):
+    if os.path.isabs(fname):
+        return fname
+    return os.path.join(dirname, fname)
+
+def depth_first_search(elems, get_outgoing):
+    visited = set()
+    to_process = list(elems)
+    while len(to_process) > 0:
+        elem = to_process.pop()
+        if elem in visited:
+            continue
+        yield elem
+        visited.add(elem)
+        to_process.extend(set(get_outgoing(elem)) - visited)
+
+def topological_sort(elems, get_outgoing):
+    data = {x:set(get_outgoing(x)) for x in elems}
+    for k, v in data.items():
+        v.discard(k) # Ignore self dependencies
+    while True:
+        ordered = set(item for item,dep in data.items() if not dep)
+        if len(ordered) == 0:
+            break
+        yield from ordered
+        data = {item: (dep - ordered) for item,dep in data.items()
+                if item not in ordered}
+    if len(data) > 0:
+        raise ValueError("A cyclic dependency exists amongst %r" % data)
+
+def make_sort(elems, get_outgoing):
+    elems = depth_first_search(elems, get_outgoing)
+    for elem in topological_sort(elems, get_outgoing):
+        yield elem
+
+class Rule:
+    def __init__(self, sources, targets, fun):
+        self.targets = targets
+        self.sources = sources
+        self.fun = fun
+        self.name = fun.__name__
+
+    def get_missing_targets(self, ctx):
+        return (x for x in self.targets if ctx.get_time(x) is None)
+
+    def needs_update(self, ctx):
+        sources = None
+        for (t_name, t_time) in map(lambda x:(x, ctx.get_time(x)), self.targets):
+            if t_time is None:
+                return True
+            # Cache getting the time of the sources
+            if sources is None:
+                sources = list(map(lambda x:(x, ctx.get_time(x)), self.sources))
+            for (s_name, s_time) in sources:
+                if ctx.target_needs_update(s_time, t_time):
+                    return True
+        return False
+
+    def run(self, ctx, *args, **kwargs):
+        if self.needs_update(ctx):
+            print("RUN", self.name)
+            self.fun(ctx, *args, **kwargs)
+        else:
+            print("CACHED", self.name)
+
+    def __repr__(self):
+        return "Rule(name=%r, sources=%r, targets=%r)" % (self.name, self.sources, self.targets)
+
+class Rules:
+    def __init__(self, get_path, elems):
+        self.get_path = get_path
+        self.rules = {}
+        for rule in elems:
+            for target in rule.targets:
+                path = get_path(target)
+                if path in self.rules:
+                    raise ValueError("Rule %r and rule %r generate the same target target %r, file %r" % (self.rules[path].name, rule.name, target, path))
+                self.rules[path] = rule
+
+    def get_rules(self, targets):
+        return (self.rules[self.get_path(x)] for x in targets)
+
+    def foreach_target(self, targets):
+        return foreach_rule(self.get_rules(targets))
+
+    def foreach_rule(self, rules):
+        return make_sort(rules, lambda x: self.get_rules(x.sources))
+
+class Makefile:
+    def __init__(self):
+        self.rules = []
+
+    def rule(self, target=None, targets=(), source=None, sources=()):
+        if target is not None:
+            targets = list(targets)
+            targets.append(target)
+        if source is not None:
+            sources = list(sources)
+            sources.append(source)
+        rules = self.rules
+        def wrapper(fun):
+            rule = Rule(sources, targets, fun)
+            rules.append(rule)
+            return rule
+
+        return wrapper
+
+    def make(self, ctx, *args, **kwargs):
+        if "targets" in kwargs:
+            targets = list(kwargs["targets"])
+            del kwargs["targets"]
+        else:
+            targets = []
+
+        if "target" in kwargs:
+            targets.append(kwargs["target"])
+            del kwargs["target"]
+
+        rules = Rules(ctx.get_path, self.rules)
+        for rule in rules.foreach_target(targets):
+            rule.run(ctx, *args, **kwargs)
+
+    def run(self, ctx, rules, *args, **kwargs):
+        db = Rules(ctx.get_path, self.rules)
+        for rule in db.foreach_rule(rules):
+            rule.run(ctx, *args, **kwargs)
+
+class EnvResolver:
+    def __init__(self, dirname, env):
+        self.dirname = dirname
+        self.env = env
+
+    def __call__(self, filename):
+        fmt = string.Formatter()
+        filename = fmt.vformat(filename, (), self.env)
+        return os.path.abspath(resolve_path(self.dirname, filename))
+
+class FileCtx:
+    def __init__(self, get_path=os.path.abspath):
+        self.get_path = get_path
+    
+    def get_time(self, path):
+        try:
+            return os.path.getmtime(self.get_path(path))
+        except FileNotFoundError:
+            return None
+
+    def target_needs_update(self, source, target):
+        if target is None:
+            return True
+        if source is None:
+            raise ValueError("Error source is null!")
+        return target < source
+
+class ArgsCtx:
+    def __init__(self, args):
+        self.args = args
+        self.get_path = Resolver(args.dirname, vars(args))
+    
+    def get_time(self, path):
+        try:
+            return os.path.getmtime(self.get_path(path))
+        except FileNotFoundError:
+            return None
+
+    def target_needs_update(self, source, target):
+        if target is None:
+            return True
+        if source is None:
+            raise ValueError("Error source is null!")
+        return target < source
